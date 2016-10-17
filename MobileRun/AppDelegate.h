cc5fb9c7ac2b9c9a503e677dc45b055313dc225f
RainyTunes <cquptrainytunes@gmail.com>
2016-10-17 13:57:02 +0800

    新建工程、初始化架构、添加代码规范和部分初期TODO

---
+++ MobileRun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileRun
+//
+//  Created by RainyTunes on 16/10/17.
+//  Copyright © 2016年 We.Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobileRun/MRLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  MRLoginViewController.h
+//  MobileRun
+//
+//  Created by RainyTunes on 16/10/17.
+//  Copyright © 2016年 We.Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRLoginViewController : UIViewController
+
+@end
